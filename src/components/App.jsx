3e2a81df282260b3c1a18d631f0366cea036b7b5
Jeff Capshaw <51003293+jcapshaw@users.noreply.github.com>
2024-02-22 20:29:25 +0000

    feat: Fixed import statements  to include createTheme from @material-ui/core/styles. and added tasks.json, devcontainer.json, and dependabot.yml files for project setup and maintenance.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import PropTypes from "prop-types";
 import React, { useState } from "react";
-import { ThemeProvider } from "@material-ui/core/styles";
-import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider, createTheme } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 import { defaultTheme } from "../styles/mui-theme";
@@ -19,7 +18,7 @@ const formatTheme = newTheme => {
   };
 };
 
-const App = ({ children }) => {
+function App({ children }) {
   const [theme, setTheme] = useState(defaultTheme);
   let muiTheme = createMuiTheme(defaultTheme);
   try {
@@ -50,7 +49,7 @@ const App = ({ children }) => {
       </ThemeProvider>
     </ThemeContext.Provider>
   );
-};
+}
 
 App.propTypes = {
   children: PropTypes.object
